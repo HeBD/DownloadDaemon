e36d3337030e6f2c7645406f7cfde4d52686ff98
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2009-10-03 09:05:53 +0000

    version 0.2

--- tools/make_version.sh
+++ tools/make_version.sh
@@ -11,9 +11,10 @@ rm -rf ../version
 # ddclient-wx...
 mkdir -p ../version/ddclient-wx-${1}/src/
 mkdir -p ../version/ddclient-wx-${1}/src/lib
+mkdir -p ../version/ddclient-wx-${1}/share
 cp -rf ../src/ddclient-wx ../version/ddclient-wx-${1}/src/
 cp -rf ../src/lib/netpptk ../version/ddclient-wx-${1}/src/lib
-cp -rf ../share ../version/ddclient-wx-${1}/
+cp -rf ../share/applications ../share/ddclient-wx ../share/doc ../share/pixmaps ../version/ddclient-wx-${1}/share/
 cp -f ../AUTHORS ../CHANGES ../TODO ../LICENCE ../INSTALLING ../version/ddclient-wx-${1}/
 echo "cmake_minimum_required (VERSION 2.6)
 
@@ -29,10 +30,11 @@ add_subdirectory(lib)" > ../version/ddclient-wx-${1}/src/CMakeLists.txt
 
 
 # DownloadDaemon
-mkdir -p ../version/downloaddaemon-${1}/src/
+mkdir -p ../version/downloaddaemon-${1}/src/ ../version/downloaddaemon-${1}/share/ ../version/downloaddaemon-${1}/etc
 cp -rf ../src/daemon ../version/downloaddaemon-${1}/src/
 cp -rf ../src/lib ../version/downloaddaemon-${1}/src/
-cp -rf ../program_data ../version/downloaddaemon-${1}/
+cp -rf ../share/downloaddaemon ../version/downloaddaemon-${1}/share/
+cp -rf ../etc/downloaddaemon ../version/downloaddaemon-${1}/etc/downloaddaemon
 cp -f ../AUTHORS ../CHANGES ../TODO ../LICENCE ../INSTALLING ../version/downloaddaemon-${1}/
 echo "cmake_minimum_required (VERSION 2.6)
 
@@ -45,21 +47,21 @@ project(DownloadDaemon)
 add_subdirectory(daemon)" > ../version/downloaddaemon-${1}/src/CMakeLists.txt
 
 
-# ddclient
-mkdir -p ../version/ddclient-${1}/src/
-mkdir -p ../version/ddclient-${1}/src/lib
-cp -rf ../src/ddclient ../version/ddclient-${1}/src
-cp -rf ../src/lib/netpptk ../version/ddclient-${1}/src/lib/
-cp -f ../AUTHORS ../CHANGES ../TODO ../LICENCE ../INSTALLING ../version/ddclient-${1}/
+# ddconsole
+mkdir -p ../version/ddconsole-${1}/src/
+mkdir -p ../version/ddconsole-${1}/src/lib
+cp -rf ../src/ddconsole ../version/ddconsole-${1}/src
+cp -rf ../src/lib/netpptk ../version/ddconsole-${1}/src/lib/
+cp -f ../AUTHORS ../CHANGES ../TODO ../LICENCE ../INSTALLING ../version/ddconsole-${1}/
 echo "cmake_minimum_required (VERSION 2.6)
 
-project(ddclient)
-add_subdirectory(src)" > ../version/ddclient-${1}/CMakeLists.txt
+project(ddconsole)
+add_subdirectory(src)" > ../version/ddconsole-${1}/CMakeLists.txt
 
 echo "cmake_minimum_required (VERSION 2.6)
 
-project(ddclient)
-add_subdirectory(ddclient)" > ../version/ddclient-${1}/src/CMakeLists.txt
+project(ddconsole)
+add_subdirectory(ddconsole)" > ../version/ddconsole-${1}/src/CMakeLists.txt
 
 
 # ddclient-php
