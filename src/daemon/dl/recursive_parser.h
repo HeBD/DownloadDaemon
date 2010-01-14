a6e524758f46f99cdb1d833c0b81da55f295ca77
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2010-01-14 13:49:54 +0000

    forgot changes in last commit

---
+++ src/daemon/dl/recursive_parser.h
@@ -0,0 +1,25 @@
+#ifndef RECURSIVE_PARSER_H_INCLUDED
+#define RECURSIVE_PARSER_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+class recursive_parser {
+public:
+	recursive_parser(std::string url);
+	void add_to_list();
+
+	static size_t to_string_callback(void *buffer, size_t size, size_t nmemb, void *userp);
+
+private:
+	void deep_parse(std::string url);
+	std::vector<std::string> parse_list(std::string list);
+	std::string get_list(std::string url);
+
+	std::string folder_url;
+
+
+
+};
+
+#endif // RECURSIVE_PARSER_H_INCLUDED
