7e6bf839b7faa3027dc837087a2e189149c5968c
koryu <koryu@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2010-03-01 20:49:45 +0000

    connect dialog & language support now fully working at ddclient-gui

--- src/ddclient-gui/ddclient_gui.cpp
+++ src/ddclient-gui/ddclient_gui.cpp
@@ -17,19 +17,248 @@
 
 using namespace std;
 
-ddclient_gui::ddclient_gui(QString config_dir) : QMainWindow(NULL), conf_dir(config_dir) {
+ddclient_gui::ddclient_gui(QString config_dir) : QMainWindow(NULL), config_dir(config_dir) {
     setWindowTitle("DownloadDaemon Client GUI");
     this->resize(750, 500);
     setWindowIcon(QIcon("img/logoDD.png"));
 
     lang.set_working_dir("lang/");
+    dclient = new downloadc();
 
     statusBar()->show();
     status_connection = new QLabel(tsl("Not connected"));
     statusBar()->insertPermanentWidget(0, status_connection);
 
     add_bars();
+    add_list_components();
+}
 
+
+ddclient_gui::~ddclient_gui(){
+    delete dclient;                                                     // TODO: <= mutexlock around this
+}
+
+
+void ddclient_gui::update_status(QString server){
+    status_connection->setText(tsl("Connected to") + " " + server);
+                                                                      // TODO: <= not finished!
+}
+
+
+QString ddclient_gui::tsl(string text, ...){
+    string translated = lang[text];
+
+    size_t n;
+    int i = 1;
+    va_list vl;
+    va_start(vl, text);
+    string search("p1");
+
+    while((n = translated.find(search)) != string::npos){
+	stringstream id;
+	id << va_arg(vl, char *);
+	translated.replace(n-1, 3, id.str());
+
+	i++;
+	if(i>9) // maximal 9 additional Parameters
+	    break;
+
+	stringstream sb;
+	sb << "p" << i;
+	search = sb.str();
+    }
+
+    return trUtf8(translated.c_str());
+}
+
+
+void ddclient_gui::set_language(std::string lang_to_set){
+    lang.set_language(lang_to_set);
+
+    update_bars();
+    update_list_components();
+}
+
+
+downloadc *ddclient_gui::get_connection(){
+    return dclient;
+}
+
+
+void ddclient_gui::add_bars(){
+    // menubar
+    file_menu = menuBar()->addMenu("&" + tsl("File"));
+    help_menu = menuBar()->addMenu("&" + tsl("Help"));
+
+    connect_action = new QAction(QIcon("img/1_connect.png"), "&" + tsl("Connect"), this);
+    connect_action->setShortcut(QString("Alt+C"));
+    connect_action->setStatusTip(tsl("Connect to a DownloadDaemon Server"));
+    file_menu->addAction(connect_action);
+
+    configure_action = new QAction(QIcon("img/8_configure.png"), tsl("Configure"), this);
+    configure_action->setShortcut(QString("Alt+P"));
+    configure_action->setStatusTip(tsl("Configure DownloadDaemon Server"));
+    file_menu->addAction(configure_action);
+
+    activate_action = new QAction(QIcon("img/9_activate.png"), tsl("Activate Downloading"), this);
+    activate_action->setShortcut(QString("F2"));
+    activate_action->setStatusTip(tsl("Activate Downloading"));
+    file_menu->addAction(activate_action);
+    activate_action->setEnabled(false);
+
+    deactivate_action = new QAction(QIcon("img/9_deactivate.png"), tsl("Deactivate Downloading"), this);
+    deactivate_action->setShortcut(QString("F3"));
+    deactivate_action->setStatusTip(tsl("Deactivate Downloading"));
+    file_menu->addAction(deactivate_action);
+    deactivate_action->setEnabled(false);
+    file_menu->addSeparator();
+
+    activate_download_action = new QAction(QIcon("img/5_start.png"), tsl("Activate Download"), this);
+    activate_download_action->setShortcut(QString("Alt+A"));
+    activate_download_action->setStatusTip(tsl("Activate the selected Download"));
+    file_menu->addAction(activate_download_action);
+
+    deactivate_download_action = new QAction(QIcon("img/4_stop.png"), tsl("Deactivate Download"), this);
+    deactivate_download_action->setShortcut(QString("Alt+D"));
+    deactivate_download_action->setStatusTip(tsl("Deactivate the selected Download"));
+    file_menu->addAction(deactivate_download_action);
+    file_menu->addSeparator();
+
+    add_action = new QAction(QIcon("img/2_add.png"), tsl("Add Download"), this);
+    add_action->setShortcut(QString("Alt+I"));
+    add_action->setStatusTip(tsl("Add Download"));
+    file_menu->addAction(add_action);
+
+    delete_action = new QAction(QIcon("img/3_delete.png"), tsl("Delete Download"), this);
+    delete_action->setShortcut(QString("DEL"));
+    delete_action->setStatusTip(tsl("Delete Download"));
+    file_menu->addAction(delete_action);
+
+    delete_finished_action = new QAction(QIcon("img/10_delete_finished.png"), tsl("Delete finished Downloads"), this);
+    delete_finished_action->setShortcut(QString("Ctrl+DEL"));
+    delete_finished_action->setStatusTip(tsl("Delete finished Downloads"));
+    file_menu->addAction(delete_finished_action);
+    file_menu->addSeparator();
+
+    select_action = new QAction(tsl("Select all"), this);
+    select_action->setShortcut(QString("Ctrl+A"));
+    select_action->setStatusTip(tsl("Select all"));
+    file_menu->addAction(select_action);
+
+    copy_action = new QAction(tsl("Copy URL"), this);
+    copy_action->setShortcut(QString("Ctrl+C"));
+    copy_action->setStatusTip(tsl("Copy URL"));
+    file_menu->addAction(copy_action);
+    file_menu->addSeparator();
+
+    quit_action = new QAction("&" + tsl("Quit"), this);
+    quit_action->setShortcut(QString("Alt+F4"));
+    quit_action->setStatusTip(tsl("Quit"));
+    file_menu->addAction(quit_action);
+
+    about_action = new QAction("&" + tsl("About"), this);
+    about_action->setShortcut(QString("F1"));
+    about_action->setStatusTip(tsl("About"));
+    help_menu->addAction(about_action);
+
+    // toolbar
+    up_action = new QAction(QIcon("img/6_up.png"), "&" + tsl("Increase Priority"), this);
+    up_action->setStatusTip(tsl("Increase Priority of the selected Download"));
+
+    down_action = new QAction(QIcon("img/7_down.png"), "&" + tsl("Dencrease Priority"), this);
+    down_action->setStatusTip(tsl("Decrease Priority of the selected Download"));
+
+    QToolBar *connect_menu = addToolBar(tsl("Connect"));
+    connect_menu->addAction(connect_action);
+
+    QToolBar *download_menu = addToolBar(tsl("Download"));
+    download_menu->addAction(add_action);
+    download_menu->addAction(delete_action);
+    download_menu->addAction(delete_finished_action);
+    download_menu->addSeparator();
+    download_menu->addAction(activate_download_action);
+    download_menu->addAction(deactivate_download_action);
+    download_menu->addSeparator();
+    download_menu->addAction(up_action);
+    download_menu->addAction(down_action);
+
+    QToolBar *configure_menu = addToolBar(tsl("Configure"));
+    configure_menu->addAction(configure_action);
+
+    downloading_menu = addToolBar(tsl("Downloading"));
+    downloading_menu->addAction(activate_action);
+
+
+    connect(connect_action, SIGNAL(triggered()), this, SLOT(on_connect()));
+    connect(configure_action, SIGNAL(triggered()), this, SLOT(on_configure()));
+    connect(activate_action, SIGNAL(triggered()), this, SLOT(on_downloading_activate()));
+    connect(deactivate_action, SIGNAL(triggered()), this, SLOT(on_downloading_deactivate()));
+    connect(activate_download_action, SIGNAL(triggered()), this, SLOT(on_activate()));
+    connect(deactivate_download_action, SIGNAL(triggered()), this, SLOT(on_deactivate()));
+    connect(add_action, SIGNAL(triggered()), this, SLOT(on_add()));
+    connect(delete_action, SIGNAL(triggered()), this, SLOT(on_delete()));
+    connect(delete_finished_action, SIGNAL(triggered()), this, SLOT(on_delete_finished()));
+    connect(select_action, SIGNAL(triggered()), this, SLOT(on_select()));
+    connect(copy_action, SIGNAL(triggered()), this, SLOT(on_copy()));
+    connect(quit_action, SIGNAL(triggered()), qApp, SLOT(quit()));
+    connect(about_action, SIGNAL(triggered()), this, SLOT(on_about()));
+    connect(up_action, SIGNAL(triggered()), this, SLOT(on_priority_up()));
+    connect(down_action, SIGNAL(triggered()), this, SLOT(on_priority_down()));
+}
+
+
+void ddclient_gui::update_bars(){
+    file_menu->setTitle("&" + tsl("File"));
+    help_menu->setTitle("&" + tsl("Help"));
+
+    connect_action->setText("&" + tsl("Connect"));
+    connect_action->setStatusTip(tsl("Connect to a DownloadDaemon Server"));
+
+    configure_action->setText(tsl("Configure"));
+    configure_action->setStatusTip(tsl("Configure DownloadDaemon Server"));
+
+    activate_action->setText(tsl("Activate Downloading"));
+    activate_action->setStatusTip(tsl("Activate Downloading"));
+
+    deactivate_action->setText(tsl("Deactivate Downloading"));
+    deactivate_action->setStatusTip(tsl("Deactivate Downloading"));
+
+    activate_download_action->setText(tsl("Activate Download"));
+    activate_download_action->setStatusTip(tsl("Activate the selected Download"));
+
+    deactivate_download_action->setText(tsl("Deactivate Download"));
+    deactivate_download_action->setStatusTip(tsl("Deactivate the selected Download"));
+
+    add_action->setText(tsl("Add Download"));
+    add_action->setStatusTip(tsl("Add Download"));
+
+    delete_action->setText(tsl("Delete Download"));
+    delete_action->setStatusTip(tsl("Delete Download"));
+
+    delete_finished_action->setText(tsl("Delete finished Downloads"));
+    delete_finished_action->setStatusTip(tsl("Delete finished Downloads"));
+
+    select_action->setText(tsl("Select all"));
+    select_action->setStatusTip(tsl("Select all"));
+
+    copy_action->setText(tsl("Copy URL"));
+    copy_action->setStatusTip(tsl("Copy URL"));
+
+    quit_action->setText("&" + tsl("Quit"));
+    quit_action->setStatusTip(tsl("Quit"));
+
+    about_action->setText("&" + tsl("About"));
+    about_action->setStatusTip(tsl("About"));
+
+    up_action->setText("&" + tsl("Increase Priority"));
+    about_action->setStatusTip(tsl("Increase Priority of the selected Download"));
+
+    down_action->setText("&" + tsl("Dencrease Priority"));
+    down_action->setStatusTip(tsl("Decrease Priority of the selected Download"));
+}
+
+
+void ddclient_gui::add_list_components(){
     // create treeview which will show all the data later
     list_model = new QStandardItemModel();
     QStringList column_labels;
@@ -87,156 +316,14 @@ ddclient_gui::ddclient_gui(QString config_dir) : QMainWindow(NULL), conf_dir(con
 }
 
 
-ddclient_gui::~ddclient_gui(){
-}
+void ddclient_gui::update_list_components(){
+    QStringList column_labels;
+    column_labels << tsl("ID") << tsl("Title") << tsl("URL") << tsl("Time left") << tsl("Status");
+    list_model->setHorizontalHeaderLabels(column_labels);
 
-
-QString ddclient_gui::tsl(string text, ...){
-    string translated = lang[text];
-
-    size_t n;
-    int i = 1;
-    va_list vl;
-    va_start(vl, text);
-    string search("p1");
-
-    while((n = translated.find(search)) != string::npos){
-	stringstream id;
-	id << va_arg(vl, char *);
-	translated.replace(n-1, 3, id.str());
-
-	i++;
-	if(i>9) // maximal 9 additional Parameters
-	    break;
-
-	stringstream sb;
-	sb << "p" << i;
-	search = sb.str();
-    }
-
-    return trUtf8(translated.c_str());
-}
-
-
-void ddclient_gui::add_bars(){
-    // menubar
-    file_menu = menuBar()->addMenu("&" + tsl("File"));
-    QMenu* help_menu = menuBar()->addMenu("&" + tsl("Help"));
-
-    QAction* connect_action = new QAction(QIcon("img/1_connect.png"), "&" + tsl("Connect"), this);
-    connect_action->setShortcut(QString("Alt+C"));
-    connect_action->setStatusTip(tsl("Connect to a DownloadDaemon Server"));
-    file_menu->addAction(connect_action);
-
-    QAction* configure_action = new QAction(QIcon("img/8_configure.png"), tsl("Configure"), this);
-    configure_action->setShortcut(QString("Alt+P"));
-    configure_action->setStatusTip(tsl("Configure DownloadDaemon Server"));
-    file_menu->addAction(configure_action);
-
-    activate_action = new QAction(QIcon("img/9_activate.png"), tsl("Activate Downloading"), this);
-    activate_action->setShortcut(QString("F2"));
-    activate_action->setStatusTip(tsl("Activate Downloading"));
-    file_menu->addAction(activate_action);
-    activate_action->setEnabled(false);
-
-    deactivate_action = new QAction(QIcon("img/9_deactivate.png"), tsl("Deactivate Downloading"), this);
-    deactivate_action->setShortcut(QString("F3"));
-    deactivate_action->setStatusTip(tsl("Deactivate Downloading"));
-    file_menu->addAction(deactivate_action);
-    deactivate_action->setEnabled(false);
-    file_menu->addSeparator();
-
-    QAction* activate_download_action = new QAction(QIcon("img/5_start.png"), tsl("Activate Download"), this);
-    activate_download_action->setShortcut(QString("Alt+A"));
-    activate_download_action->setStatusTip(tsl("Activate the selected Download"));
-    file_menu->addAction(activate_download_action);
-
-    QAction* deactivate_download_action = new QAction(QIcon("img/4_stop.png"), tsl("Deactivate Download"), this);
-    deactivate_download_action->setShortcut(QString("Alt+D"));
-    deactivate_download_action->setStatusTip(tsl("Deactivate the selected Download"));
-    file_menu->addAction(deactivate_download_action);
-    file_menu->addSeparator();
-
-    QAction* add_action = new QAction(QIcon("img/2_add.png"), tsl("Add Download"), this);
-    add_action->setShortcut(QString("Alt+I"));
-    add_action->setStatusTip(tsl("Add Download"));
-    file_menu->addAction(add_action);
-
-    QAction* delete_action = new QAction(QIcon("img/3_delete.png"), tsl("Delete Download"), this);
-    delete_action->setShortcut(QString("DEL"));
-    delete_action->setStatusTip(tsl("Delete Download"));
-    file_menu->addAction(delete_action);
-
-    QAction* delete_finished_action = new QAction(QIcon("img/10_delete_finished.png"), tsl("Delete finished Downloads"), this);
-    delete_finished_action->setShortcut(QString("Ctrl+DEL"));
-    delete_finished_action->setStatusTip(tsl("Delete finished Downloads"));
-    file_menu->addAction(delete_finished_action);
-    file_menu->addSeparator();
-
-    QAction* select_action = new QAction(tsl("Select all"), this);
-    select_action->setShortcut(QString("Ctrl+A"));
-    select_action->setStatusTip(tsl("Select all"));
-    file_menu->addAction(select_action);
-
-    QAction* copy_action = new QAction(tsl("Copy URL"), this);
-    copy_action->setShortcut(QString("Ctrl+C"));
-    copy_action->setStatusTip(tsl("Copy URL"));
-    file_menu->addAction(copy_action);
-    file_menu->addSeparator();
-
-    QAction* quit_action = new QAction("&" + tsl("Quit"), this);
-    quit_action->setShortcut(QString("Alt+F4"));
-    quit_action->setStatusTip(tsl("Quit"));
-    file_menu->addAction(quit_action);
-
-    QAction* about_action = new QAction("&" + tsl("About"), this);
-    about_action->setShortcut(QString("F1"));
-    about_action->setStatusTip(tsl("About"));
-    help_menu->addAction(about_action);
-
-    // toolbar
-    QAction* up_action = new QAction(QIcon("img/6_up.png"), "&" + tsl("Increase Priority"), this);
-    about_action->setStatusTip(tsl("Increase Priority of the selected Download"));
-
-    QAction* down_action = new QAction(QIcon("img/7_down.png"), "&" + tsl("Dencrease Priority"), this);
-    about_action->setStatusTip(tsl("Decrease Priority of the selected Download"));
-
-    QToolBar *connect_menu = addToolBar(tsl("Connect"));
-    connect_menu->addAction(connect_action);
-
-    QToolBar *download_menu = addToolBar(tsl("Download"));
-    download_menu->addAction(add_action);
-    download_menu->addAction(delete_action);
-    download_menu->addAction(delete_finished_action);
-    download_menu->addSeparator();
-    download_menu->addAction(activate_download_action);
-    download_menu->addAction(deactivate_download_action);
-    download_menu->addSeparator();
-    download_menu->addAction(up_action);
-    download_menu->addAction(down_action);
-
-    QToolBar *configure_menu = addToolBar(tsl("Configure"));
-    configure_menu->addAction(configure_action);
-
-    downloading_menu = addToolBar(tsl("Downloading"));
-    downloading_menu->addAction(activate_action);
-
-
-    connect(connect_action, SIGNAL(triggered()), this, SLOT(on_connect()));
-    connect(configure_action, SIGNAL(triggered()), this, SLOT(on_configure()));
-    connect(activate_action, SIGNAL(triggered()), this, SLOT(on_downloading_activate()));
-    connect(deactivate_action, SIGNAL(triggered()), this, SLOT(on_downloading_deactivate()));
-    connect(activate_download_action, SIGNAL(triggered()), this, SLOT(on_activate()));
-    connect(deactivate_download_action, SIGNAL(triggered()), this, SLOT(on_deactivate()));
-    connect(add_action, SIGNAL(triggered()), this, SLOT(on_add()));
-    connect(delete_action, SIGNAL(triggered()), this, SLOT(on_delete()));
-    connect(delete_finished_action, SIGNAL(triggered()), this, SLOT(on_delete_finished()));
-    connect(select_action, SIGNAL(triggered()), this, SLOT(on_select()));
-    connect(copy_action, SIGNAL(triggered()), this, SLOT(on_copy()));
-    connect(quit_action, SIGNAL(triggered()), qApp, SLOT(quit()));
-    connect(about_action, SIGNAL(triggered()), this, SLOT(on_about()));
-    connect(up_action, SIGNAL(triggered()), this, SLOT(on_priority_up()));
-    connect(down_action, SIGNAL(triggered()), this, SLOT(on_priority_down()));
+    /*mx.lock();
+    content.clear(); // delete old content to force reload of list
+    mx.unlock();*/
 }
 
 
@@ -276,7 +363,7 @@ void ddclient_gui::on_select(){
 
 
 void ddclient_gui::on_connect(){
-    connect_dialog dialog(this);
+    connect_dialog dialog(this, config_dir);
     dialog.setModal(true);
     dialog.exec();
 }
@@ -351,36 +438,19 @@ void ddclient_gui::on_reload(){
 void ddclient_gui::contextMenuEvent(QContextMenuEvent *event){
     QMenu menu(this);
 
-    QAction* activate_download_action = new QAction(tsl("Activate Download"), this);
-    activate_download_action->setShortcut(QString("Alt+A"));
-    activate_download_action->setStatusTip(tsl("Activate Download"));
-    menu.addAction(activate_download_action);
-
-    QAction* deactivate_download_action = new QAction(tsl("Deactivate Download"), this);
-    deactivate_download_action->setShortcut(QString("Alt+D"));
-    deactivate_download_action->setStatusTip(tsl("Deactivate Download"));
-    menu.addAction(deactivate_download_action);
-    menu.addSeparator();
-
-    QAction* delete_action = new QAction(tsl("Delete Download"), this);
-    delete_action->setShortcut(QString("DEL"));
-    delete_action->setStatusTip(tsl("Delete Download"));
-    menu.addAction(delete_action);
-
     QAction* delete_file_action = new QAction(tsl("Delete File"), this);
     delete_file_action->setShortcut(QString("Ctrl+F"));
     delete_file_action->setStatusTip(tsl("Delete File"));
     menu.addAction(delete_file_action);
     menu.addSeparator();
 
-    QAction* select_action = new QAction(tsl("Select all"), this);
-    select_action->setShortcut(QString("Ctrl+A"));
-    select_action->setStatusTip(tsl("Select all"));
+    menu.addAction(activate_download_action);
+    menu.addAction(deactivate_download_action);
+    menu.addSeparator();
+    menu.addAction(delete_action);
+    menu.addAction(delete_file_action);
+    menu.addSeparator();
     menu.addAction(select_action);
-
-    QAction* copy_action = new QAction(tsl("Copy URL"), this);
-    copy_action->setShortcut(QString("Ctrl+C"));
-    copy_action->setStatusTip(tsl("Copy URL"));
     menu.addAction(copy_action);
 
     connect(activate_download_action, SIGNAL(triggered()), this, SLOT(on_activate()));
--- src/ddclient-gui/ddclient_gui.h
+++ src/ddclient-gui/ddclient_gui.h
@@ -22,6 +22,14 @@ struct selected_info{
 };
 
 
+struct login_data{
+        char host[256];
+        int port;
+        char pass[256];
+        char lang[128];
+};
+
+
 class ddclient_gui : public QMainWindow{
     Q_OBJECT
 
@@ -32,27 +40,61 @@ class ddclient_gui : public QMainWindow{
 	/** Destructor */
 	~ddclient_gui();
 
+        /** Updates Status of the Program, should be called after Connecting to a Server
+        *    @param server Location of the Server, which the client is connected to
+        */
+        void update_status(QString server);
+
 	/** Translates a string and changes it into a wxString
 	*    @param text string to translate
 	*    @returns translated wxString
 	*/
 	QString tsl(std::string text, ...);
 
+        /** Setter for Language
+        *    @param lang_to_set Language
+        */
+        void set_language(std::string lang_to_set);
+
+        /** Getter for Download-Client Object (used for Communication with Daemon)
+        *    @returns Download-Client Object
+        */
+        downloadc *get_connection();
+
     private:
         void add_bars();
+        void update_bars();
+        void add_list_components();
+        void update_list_components();
         void get_selected_lines();
 
-        QMenu *file_menu;
-        QToolBar *downloading_menu;
-        QAction *activate_action;
-        QAction *deactivate_action;
-        QLabel *status_connection;
         QTreeView *list;
         QStandardItemModel *list_model;
         QItemSelectionModel *selection_model;
+        QLabel *status_connection;
 
-        QString conf_dir;
+        QMenu *file_menu;
+        QMenu *help_menu;
+        QAction *connect_action;
+        QAction *configure_action;
+        QAction *activate_action;
+        QAction *deactivate_action;
+        QAction *activate_download_action;
+        QAction *deactivate_download_action;
+        QAction *add_action;
+        QAction *delete_action;
+        QAction *delete_finished_action;
+        QAction *select_action;
+        QAction *copy_action;
+        QAction *quit_action;
+        QAction *about_action;
+        QAction *down_action;
+        QAction *up_action;
+        QToolBar *downloading_menu;
+
+        QString config_dir;
         language lang;
+        downloadc *dclient;
         std::vector<selected_info> selected_lines;
 
     private slots:
