64bc1818cb2484d7597837b7ea7d62a849ebe0f8
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2010-03-09 19:18:16 +0000

    - auto-refresh only refreshs if there are downloads with the status DOWNLOAD_RUNNING or DOWNLOAD_WAITING

--- trunk/src/ddclient-php/inc/_config.inc.php
+++ trunk/src/ddclient-php/inc/_config.inc.php
@@ -1,7 +1,29 @@
 <?php
+/*
+ Sets the default host that will be seen on the login page
+*/
 define('DEFAULT_HOST', '127.0.0.1');
+
+/*
+ Sets the default port that will be seen on the login page
+*/
 define('DEFAULT_PORT', '56789');
+
+/*
+ Sets the template/theme that should be used for displaying. Right now there is only 'default'
+*/
 define('TEMPLATE', 'default');
+
+/*
+ Sets the language that should be used. Right now there is 'en' and 'de', but not all parts will be
+ translated yet, if you set something else than 'en'. But feel free to try it out.
+*/
 define('LANG', 'en');
-define('AUTO_REFRESH', false);
+
+/*
+ If this is set to true, ddclient-php will automatically refresh the displayed download list if
+ there are running downloads. If no download is running, the page will not reload, so it doesn't
+ prevent the machine where DownloadDaemon runs on from spinning down the hard-disk.
+*/
+define('AUTO_REFRESH', true);
 ?>
