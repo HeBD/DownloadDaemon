a02035fc4a7d0f44ae3d21c62a97821ce622d402
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2010-03-23 20:38:11 +0000

    fixed missing includes and added missing GPL headers

--- src/daemon/dl/package_extractor.h
+++ src/daemon/dl/package_extractor.h
@@ -1,8 +1,17 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef PACKAGE_EXTRACTOR_H_INCLUDED
 #define PACKAGE_EXTRACTOR_H_INCLUDED
 #include <string>
-
-
 
 class pkg_extractor {
 public:
