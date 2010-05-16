0fb8faf88828d6793bd79a93ccaabc40cb92135a
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2010-05-16 14:19:17 +0000

    updated build-scripts, etc

--- src/include/config.h
+++ src/include/config.h
@@ -1,12 +1,32 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define USE_STD_THREAD
 #define DD_CONF_DIR "/etc/downloaddaemon/"
+#define DOWNLOADDAEMON
+#define DOWNLOADDAEMON_VERSION "r753"
+
 #define HAVE_UINT64_T
 #define HAVE_SYSLOG_H
 #define HAVE_INITGROUPS
+#define HAVE_STAT64
+#define USE_STD_THREAD
+
 #define BACKTRACE_ON_CRASH
+
+#ifdef __CYGWIN__
+	#ifndef RTLD_LOCAL
+		#define RTLD_LOCAL 0
+	#endif
+#endif
+
+#ifdef HAVE_STAT64
+        #define pstat stat64
+#else
+        #define pstat stat
+#endif
+
+#define DDCLIENT_GUI
+#define DDCLIENT_GUI_VERSION "r753"
 
 
 #endif // CONFIG_H
