6dbcdecae1638f7be4c4c3fec74357fe343f4180
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2010-10-04 08:17:23 +0000

    added project-support and flattr-button to ddclient-gui's toolbar

--- trunk/src/ddclient-gui/ddclient_gui.h
+++ trunk/src/ddclient-gui/ddclient_gui.h
@@ -186,7 +186,7 @@ class ddclient_gui : public QMainWindow{
         QAction *about_action;
         QAction *down_action;
         QAction *up_action;
-        QToolBar *downloading_menu;
+		QToolBar *configure_menu;
         QMenu *tray_menu;
         QSystemTrayIcon *tray_icon;
 
@@ -213,6 +213,8 @@ class ddclient_gui : public QMainWindow{
         void on_load_container();
         void on_activate_tray_icon(QSystemTrayIcon::ActivationReason reason);
         void on_reload();
+		void donate_flattr();
+		void donate_sf();
 
     protected:
         void contextMenuEvent(QContextMenuEvent *event);
