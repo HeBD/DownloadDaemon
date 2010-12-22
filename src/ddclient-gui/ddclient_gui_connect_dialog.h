f23c23c981268eb609db1ceab5a41295f3f919a0
koryu <koryu@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2010-12-22 09:04:59 +0000

    fixed bug at ddclient-gui: list cleared when connecting to the same server and something was selected

--- src/ddclient-gui/ddclient_gui_connect_dialog.h
+++ src/ddclient-gui/ddclient_gui_connect_dialog.h
@@ -24,24 +24,24 @@ class connect_dialog : public QDialog{
     Q_OBJECT
 
     public:
-        /** Constructor
-        *   @param parent MainWindow, which calls the dialog
-        *   @param config_dir Configuration Directory
-        */
-        connect_dialog(QWidget *parent, QString config_dir);
+		/** Constructor
+		*   @param parent MainWindow, which calls the dialog
+		*   @param config_dir Configuration Directory
+		*/
+		connect_dialog(QWidget *parent, QString config_dir);
 
-    private:
-	QComboBox *host;
-        QLineEdit *port;
-        QLineEdit *password;
-        QComboBox *language;
-        QCheckBox *save_data;
-        QString config_dir;
-	cfgfile file;
-	login_data data;
+		private:
+		QComboBox *host;
+		QLineEdit *port;
+		QLineEdit *password;
+		QComboBox *language;
+		QCheckBox *save_data;
+		QString config_dir;
+		cfgfile file;
+		login_data data;
 
     private slots:
-	void host_selected();
+		void host_selected();
         void ok();
 
 };
