ddcf2477208bb467ee84f49d230d84b4391eb321
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2011-01-07 01:28:53 +0000

    Recreating angelscript branch

---
+++ branches/as_plugins/src/daemon/plugins/fileserve_com.cpp
@@ -0,0 +1,94 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#define PLUGIN_CAN_PRECHECK
+#define PLGFILE fileserve_com
+#include "plugin_helpers.h"
+#include <curl/curl.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+	std::string *blubb = (std::string*)userp;
+	blubb->append((char*)buffer, nmemb);
+	return nmemb;
+}
+
+plugin_status plugin_exec(plugin_input &inp, plugin_output &outp) {
+	if(inp.premium_user.empty() || inp.premium_password.empty()) {
+		return PLUGIN_AUTH_FAIL; // free download not supported yet
+	}
+	string result;
+	ddcurl* handle = get_handle();
+	string data = "loginUserName=" + handle->escape(inp.premium_user) + "&loginUserPassword=" + handle->escape(inp.premium_password);
+	data += "+&autoLogin=on&recaptcha_response_field=&recaptcha_challenge_field=&recaptcha_shortencode_field=&loginFormSubmit=Login";
+	handle->setopt(CURLOPT_URL, "http://www.fileserve.com/login.php");
+	handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+	handle->setopt(CURLOPT_WRITEDATA, &result);
+	handle->setopt(CURLOPT_COPYPOSTFIELDS, data.c_str());
+	handle->setopt(CURLOPT_COOKIEFILE, "");
+	int ret = handle->perform();
+	if(ret != CURLE_OK)
+		return PLUGIN_CONNECTION_ERROR;
+	handle->setopt(CURLOPT_FOLLOWLOCATION, 1);
+	outp.download_url = get_url();
+	return PLUGIN_SUCCESS;
+}
+
+bool get_file_status(plugin_input &inp, plugin_output &outp) {
+	ddcurl handle;
+	string result;
+	handle.setopt(CURLOPT_URL, inp.url);
+	handle.setopt(CURLOPT_WRITEFUNCTION, write_data);
+	handle.setopt(CURLOPT_WRITEDATA, &result);
+	int ret = handle.perform();
+	if(ret != CURLE_OK) return true;
+	result = search_between(result, "<div class=\"panel file_download\">", "<a class=\"addthis_button\"");
+	if(result.empty()) {
+		outp.file_online = PLUGIN_FILE_NOT_FOUND;
+		return true;
+	}
+	try {
+		outp.download_filename = search_between(result, "<h1>", "<br/>");
+		string size_total = search_between(result, "left;\"><strong>", "</strong>");
+		vector<string> size_split = split_string(size_total, " ");
+		filesize_t size = string_to_long(size_split[0]);
+
+		if(size_split[1] == "MB")
+			size *= 1024*1024;
+		else if(size_split[1] == "KB")
+			size *= 1024;
+		else if(size_split[1] == "GB")
+			size *= 1024*1024*1024;
+
+		outp.file_size = size;
+	} catch(...) {
+		outp.file_online = PLUGIN_FILE_NOT_FOUND;
+		return true;
+	}
+
+
+
+	return true;
+}
+
+extern "C" void plugin_getinfo(plugin_input &inp, plugin_output &outp) {
+	if(!inp.premium_user.empty() && !inp.premium_password.empty()) {
+		outp.allows_resumption = true;
+		outp.allows_multiple = true;
+	} else {
+		outp.allows_resumption = false;
+		outp.allows_multiple = false;
+	}
+	outp.offers_premium = true;
+}
+
---
+++ branches/as_plugins/src/ddclient-gui/ddclient-gui.pro
@@ -0,0 +1,37 @@
+HEADERS += ddclient_gui.h \
+    ../include/downloadc/client_exception.h \
+    ../include/downloadc/client_exception.h \
+    ../include/downloadc/downloadc.h \
+    ../include/netpptk/netpptk.h \
+    ../include/crypt/md5.h \
+    ../include/language/language.h \
+    ddclient_gui_connect_dialog.h \
+    ddclient_gui_update_thread.h \
+    ddclient_gui_add_dialog.h \
+    ddclient_gui_about_dialog.h \
+    ddclient_gui_configure_dialog.h \
+    ../include/cfgfile/cfgfile.h \
+    ddclient_gui_status_bar.h
+SOURCES += ddclient_gui.cpp \
+    main.cpp \
+    ../include/downloadc/downloadc.cpp \
+    ../include/downloadc/client_exception.cpp \
+    ../include/netpptk/netpptk.cpp \
+    ../include/crypt/md5.cpp \
+    ../include/language/language.cpp \
+    ddclient_gui_connect_dialog.cpp \
+    ddclient_gui_update_thread.cpp \
+    ddclient_gui_add_dialog.cpp \
+    ddclient_gui_about_dialog.cpp \
+    ddclient_gui_configure_dialog.cpp \
+    ../include/cfgfile/cfgfile.cpp \
+    ddclient_gui_status_bar.cpp
+INCLUDEPATH += ../include C:\Boost\include\boost-1_42
+DESTDIR = ../../build/bin
+
+win32 { 
+    LIBS += -lws2_32
+    RC_FILE = ddgui.rc
+}
+
+QMAKE_CXXFLAGS += -std=c++0x -DDDCLIENT_GUI
