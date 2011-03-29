13a50dc0d2ccfb4520d7ab66bff7f8ec18a939ed
Boy1979 <Boy1979@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2011-03-29 18:05:05 +0000

    shragle.com bugfix / added new share.cx Plugin

--- trunk/src/daemon/plugins/shragle_com.cpp
+++ trunk/src/daemon/plugins/shragle_com.cpp
@@ -27,7 +27,7 @@ plugin_status plugin_exec(plugin_input &inp, plugin_output &outp) {
         std::string resultstr;
         string url = get_url();
 
-		// Seite einlesen und in resultstr speichern
+                // Seite einlesen und in resultstr speichern
         handle->setopt(CURLOPT_URL, url.c_str());
         handle->setopt(CURLOPT_COOKIEFILE, "");
         handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
@@ -36,26 +36,33 @@ plugin_status plugin_exec(plugin_input &inp, plugin_output &outp) {
         if(success != 0) {
                 return PLUGIN_CONNECTION_ERROR;
         }
+        size_t waitpos = resultstr.find("You have download 1 File in the last");
+        string waittime = search_between(resultstr, "Please wait ", " miniuts");
+        if(waitpos != string::npos) {
+            set_wait_time(atoi(waittime.c_str())*60);
+          return PLUGIN_LIMIT_REACHED;
+         }
 
-		//get the post-data/real link
+
+                //get the post-data/real link
         string fileID = search_between(resultstr, "name=\"fileID\" value=\"", "\"");
         string dlSession = handle->escape(search_between(resultstr, "name=\"dlSession\" value=\"","\""));
         string userID = search_between(resultstr, "name=\"userID\" value=\"","\"");
         string password = search_between(resultstr, "name=\"password\" value=\"","\"");
         string lang = search_between(resultstr, "name=\"lang\" value=\"","\"");
-		string postdata ="fileID=" + fileID + "&dlSession=" + dlSession + "&userID=" + userID + "&password=" + password + "&lang=" + lang;
-		url       = search_between(resultstr, "id=\"download\" action=\"", "\"");
+                string postdata ="fileID=" + fileID + "&dlSession=" + dlSession + "&userID=" + userID + "&password=" + password + "&lang=" + lang;
+                url       = search_between(resultstr, "id=\"download\" action=\"", "\"");
 
 
-		set_wait_time(60);
+                set_wait_time(60);
         resultstr.clear();
-		// prepare handle for DD
+                // prepare handle for DD
         handle->setopt(CURLOPT_HTTPPOST,1);
         handle->setopt(CURLOPT_FOLLOWLOCATION, 1);
         handle->setopt(CURLOPT_URL, url.c_str());
         handle->setopt(CURLOPT_COPYPOSTFIELDS, postdata);
-		outp.download_url = url;
-		return PLUGIN_SUCCESS;
+                outp.download_url = url;
+                return PLUGIN_SUCCESS;
 
 }
 
@@ -64,8 +71,8 @@ extern "C" void plugin_getinfo(plugin_input &inp, plugin_output &outp) {
                 outp.allows_resumption = true;
                 outp.allows_multiple = true;
         } else {
-				outp.allows_resumption = false;
-				outp.allows_multiple = false;
+                                outp.allows_resumption = false;
+                                outp.allows_multiple = false;
         }
-		outp.offers_premium = false;
+                outp.offers_premium = false;
 }
