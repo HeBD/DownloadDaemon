d549ef7651afcf281e254c1c57ffe5a3cd8a1a47
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2011-08-18 18:45:41 +0000

    corrected indents

--- trunk/src/daemon/dl/package_extractor.cpp
+++ trunk/src/daemon/dl/package_extractor.cpp
@@ -28,42 +28,42 @@ using std::string;
 
 pkg_extractor::extract_status pkg_extractor::extract_package(const std::string& filename, const std::string& password) {
 	tool to_use = required_tool(filename);
-        pkg_extractor::extract_status extract_status;
+	pkg_extractor::extract_status extract_status;
 	switch(to_use) {
 		case GNU_UNRAR:
 		case RARLAB_UNRAR:
-                        extract_status =  extract_rar(filename, password, to_use);
-                        break;
+			extract_status =  extract_rar(filename, password, to_use);
+			break;
 		case TAR:
 		case TAR_BZ2:
 		case TAR_GZ:
-                        extract_status = extract_tar(filename, to_use);
-                        break;
+			extract_status = extract_tar(filename, to_use);
+			break;
 		case ZIP:
-                        extract_status = extract_zip(filename, password, to_use);
-                        break;
-                case HJSPLIT:
-                        extract_status = merge_hjsplit(filename, to_use);
-                        break;
+			extract_status = extract_zip(filename, password, to_use);
+			break;
+		case HJSPLIT:
+			extract_status = merge_hjsplit(filename, to_use);
+			break;
 
 		default:
-                        return PKG_INVALID;
+			return PKG_INVALID;
 	}
-        //deep extract
-        string targetDir = getTargetDir(filename,to_use);
-        log_string("Deep unrar: target dir=" + targetDir,LOG_DEBUG);
-        std::vector<std::string> files=getDir(targetDir);
-        if(!files.empty())
-        {
-            for(size_t i = 0; i < files.size(); i++)
-            {
-                log_string("Trying to deep extract "+ files[i],LOG_DEBUG);
-                pkg_extractor::extract_status temp=extract_package(files[i],password);
-                if(temp!=PKG_INVALID)
-                    extract_status = temp;
-            }
-        }
-        return extract_status;
+	//deep extract
+	string targetDir = getTargetDir(filename,to_use);
+	log_string("Deep unrar: target dir=" + targetDir,LOG_DEBUG);
+	std::vector<std::string> files=getDir(targetDir);
+	if(!files.empty())
+	{
+		for(size_t i = 0; i < files.size(); i++)
+		{
+			log_string("Trying to deep extract "+ files[i],LOG_DEBUG);
+			pkg_extractor::extract_status temp=extract_package(files[i],password);
+			if(temp!=PKG_INVALID)
+				extract_status = temp;
+		}
+	}
+	return extract_status;
 }
 
 pkg_extractor::tool pkg_extractor::required_tool(std::string filename) {
@@ -93,9 +93,9 @@ pkg_extractor::tool pkg_extractor::required_tool(std::string filename) {
 		return ZIP;
 	}
 
-        if((n = filename.find(".001")) == filename.size() - 4 && n != string::npos) {
-                return HJSPLIT;
-        }
+	if((n = filename.find(".001")) == filename.size() - 4 && n != string::npos) {
+		return HJSPLIT;
+	}
 
 	return NONE;
 }
@@ -159,7 +159,7 @@ pkg_extractor::extract_status pkg_extractor::extract_rar(const std::string& file
 	char fn_path[FILENAME_MAX];
 	strncpy(fn_path, filename.c_str(), FILENAME_MAX);
 	char target_dir[FILENAME_MAX];
-        strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
+	strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
 	char unrar_pw[512] = "-p";
 	strncpy(unrar_pw + 2, password.c_str(), 510);
 
@@ -241,7 +241,7 @@ pkg_extractor::extract_status pkg_extractor::extract_tar(const std::string& file
 	strncpy(fn_path, filename.c_str(), FILENAME_MAX);
 	char target_dir[FILENAME_MAX];
 
-        strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
+	strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
 
 	tar_path[FILENAME_MAX - 1] = '\0';
 	fn_path[FILENAME_MAX - 1] = '\0';
@@ -290,8 +290,8 @@ pkg_extractor::extract_status pkg_extractor::extract_tar(const std::string& file
 }
 
 pkg_extractor::extract_status pkg_extractor::extract_zip(const std::string& filename, const std::string& password, tool t) {
-        //std::string unzip_path_s = "/usr/bin/unzip";
-        std::string unzip_path_s = global_config.get_cfg_value("unzip_path");
+	//std::string unzip_path_s = "/usr/bin/unzip";
+ 	std::string unzip_path_s = global_config.get_cfg_value("unzip_path");
 	if(unzip_path_s.empty()) return PKG_ERROR;
 	pid_t child_id;
 	char buf[256];
@@ -304,7 +304,7 @@ pkg_extractor::extract_status pkg_extractor::extract_zip(const std::string& file
 	char pass[512];
 	strncpy(pass, password.c_str(), 512);
 
-        strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
+	strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
 
 	unzip_path[FILENAME_MAX - 1] = '\0';
 	fn_path[FILENAME_MAX - 1] = '\0';
@@ -358,94 +358,97 @@ pkg_extractor::extract_status pkg_extractor::extract_zip(const std::string& file
 
 pkg_extractor::extract_status pkg_extractor::merge_hjsplit(const std::string& filename, tool t)
 {
-    std::string hjsplit_path_s = global_config.get_cfg_value("hjsplit_path");
-    if(hjsplit_path_s.empty()) return PKG_ERROR;
-    pid_t child_id;
-    char buf[256];
+	std::string hjsplit_path_s = global_config.get_cfg_value("hjsplit_path");
+	if(hjsplit_path_s.empty()) return PKG_ERROR;
+	pid_t child_id;
+	char buf[256];
 
-    char hjsplit_path[FILENAME_MAX];
-    strncpy(hjsplit_path, hjsplit_path_s.c_str(), FILENAME_MAX);
-    char fn_path[FILENAME_MAX];
-    strncpy(fn_path, filename.c_str(), FILENAME_MAX);
-    char target_dir[FILENAME_MAX];
+	char hjsplit_path[FILENAME_MAX];
+	strncpy(hjsplit_path, hjsplit_path_s.c_str(), FILENAME_MAX);
+	char fn_path[FILENAME_MAX];
+	strncpy(fn_path, filename.c_str(), FILENAME_MAX);
+	char target_dir[FILENAME_MAX];
 
-    strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
+	strncpy(target_dir, getTargetDir(filename,t).c_str(), FILENAME_MAX);
 
-    hjsplit_path[FILENAME_MAX - 1] = '\0';
-    fn_path[FILENAME_MAX - 1] = '\0';
-    target_dir[FILENAME_MAX - 1] = '\0';
+	hjsplit_path[FILENAME_MAX - 1] = '\0';
+	fn_path[FILENAME_MAX - 1] = '\0';
+	target_dir[FILENAME_MAX - 1] = '\0';
 
-    int ctop[2];
-    if(pipe(ctop) != 0) {
-            log_string("Failed to open pipe while trying to merge a hjsplit-package", LOG_ERR);
-            return PKG_ERROR;
-    }
-    mkdir_recursive(target_dir);
-    if((child_id = fork()) == 0) {
-            close(ctop[0]);
-            dup2(ctop[1], STDOUT_FILENO);
-            dup2(ctop[1], STDERR_FILENO);
-            // NO LOGGIN HERE! any complex operation in a forked child in a multithreaded application is VERY
-            // dangerous. This inclues simple stuff, like using STL containers or C standard functions
-            // log_string("excuting hjsplit path:" + hjsplit_path_s + "\nfn_path:" + filename, LOG_DEBUG);
-            chdir(target_dir);
-            execlp(hjsplit_path, hjsplit_path, "-j", fn_path, NULL);
+	int ctop[2];
+	if(pipe(ctop) != 0) {
+		log_string("Failed to open pipe while trying to merge a hjsplit-package", LOG_ERR);
+		return PKG_ERROR;
+	}
+	mkdir_recursive(target_dir);
+	if((child_id = fork()) == 0) {
+		close(ctop[0]);
+		dup2(ctop[1], STDOUT_FILENO);
+		dup2(ctop[1], STDERR_FILENO);
+		// NO LOGGIN HERE! any complex operation in a forked child in a multithreaded application is VERY
+		// dangerous. This inclues simple stuff, like using STL containers or C standard functions
+		// log_string("excuting hjsplit path:" + hjsplit_path_s + "\nfn_path:" + filename, LOG_DEBUG);
+		chdir(target_dir);
+		execlp(hjsplit_path, hjsplit_path, "-j", fn_path, NULL);
 
-            exit(-1);
+		exit(-1);
+	} else {
+		// parent
+		close(ctop[1]);
 
-    } else {
-            // parent
-            close(ctop[1]);
+	} else {
+		// parent
+		close(ctop[1]);
 
-            std::string result;
-            size_t num;
-            // we don't need the output. we just ignore it.
-            while((num = read(ctop[0], buf, 256)) > 0)
-            {
-                 result.append(buf, num);
-            }
-            //log_string("result = "+result,LOG_DEBUG);
-            int retval;
-            waitpid(child_id, &retval, 0);
-            if(retval == 0) {
-                log_string("HJSPLIT: package merged succesfully",LOG_DEBUG);
-                    return PKG_SUCCESS;
-            }
-    }
-    return PKG_ERROR;
+		std::string result;
+		size_t num;
+		// we don't need the output. we just ignore it.
+		while((num = read(ctop[0], buf, 256)) > 0)
+		{
+			result.append(buf, num);
+		}
+		//log_string("result = "+result,LOG_DEBUG);
+		int retval;
+		waitpid(child_id, &retval, 0);
+		if(retval == 0) {
+			log_string("HJSPLIT: package merged succesfully",LOG_DEBUG);
+			return PKG_SUCCESS;
+		}
+	}
+	return PKG_ERROR;
 }
 
 string pkg_extractor::getTargetDir(const std::string &filename, tool t)
 {
-    size_t ext_len=0;
-    switch(t)
-    {
-        case GNU_UNRAR:
-        case RARLAB_UNRAR:
-        case ZIP:
-        case HJSPLIT:
-        case TAR: ext_len = 4; break;
-        case TAR_GZ: ext_len = 7; break;
-        case TAR_BZ2: ext_len = 8; break;
-        default: ext_len = 0;
-    }
-    return filename.substr(0, filename.size() - ext_len);
+	size_t ext_len=0;
+	switch(t)
+	{
+		case GNU_UNRAR:
+		case RARLAB_UNRAR:
+		case ZIP:
+		case HJSPLIT:
+		case TAR: ext_len = 4; break;
+		case TAR_GZ: ext_len = 7; break;
+		case TAR_BZ2: ext_len = 8; break;
+		default: ext_len = 0;
+	}
+	return filename.substr(0, filename.size() - ext_len);
 }
 
 std::vector<std::string> pkg_extractor::getDir(std::string dir)
 {
-    DIR *dp;
-    std::vector<std::string> files = std::vector<std::string>();
-    struct dirent *dirp;
-    if((dp  = opendir(dir.c_str())) == NULL) {
-        log_string("cannot open dir" + dir,LOG_DEBUG);
-        return files;
-    }
+	DIR *dp;
+	std::vector<std::string> files = std::vector<std::string>();
+	struct dirent *dirp;
+	if((dp  = opendir(dir.c_str())) == NULL) {
+		log_string("cannot open dir" + dir,LOG_DEBUG);
+		return files;
+	}
 
-    while ((dirp = readdir(dp)) != NULL) {
-        files.push_back(dir + "/" + string(dirp->d_name));
-    }
-    closedir(dp);
-    return files;
+	while ((dirp = readdir(dp)) != NULL) {
+		files.push_back(dir + "/" + string(dirp->d_name));
+	}
+	closedir(dp);
+	return files;
 }
 
