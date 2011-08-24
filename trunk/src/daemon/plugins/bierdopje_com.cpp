373208c1a37dbec2dd6edb90a27dfe8c68e2bea4
boudcallens <boudcallens@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2011-08-24 19:10:15 +0000

    adjusting plugins to specs Adrian

--- trunk/src/daemon/plugins/bierdopje_com.cpp
+++ trunk/src/daemon/plugins/bierdopje_com.cpp
@@ -9,7 +9,6 @@
  * GNU General Public License for more details.
  */
 
-#define PLUGIN_CAN_PRECHECK
 #include "plugin_helpers.h"
 #include <curl/curl.h>
 #include <cstdlib>
@@ -105,11 +104,6 @@ plugin_status plugin_exec(plugin_input &inp, plugin_output &outp) {
 		outp.download_url=url;
 		return PLUGIN_SUCCESS;
 	}
-}
-
-bool get_file_status(plugin_input &inp, plugin_output &outp)
-{
-	return false;
 }
 
 extern "C" void plugin_getinfo(plugin_input &inp, plugin_output &outp)
