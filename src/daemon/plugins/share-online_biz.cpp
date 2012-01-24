192d7cd07e5bb1ae7354fd42a7b19fa36550b79d
boy1979 <boy1979@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2012-01-24 19:36:08 +0000

    enable premium support for share-online_biz

--- src/daemon/plugins/share-online_biz.cpp
+++ src/daemon/plugins/share-online_biz.cpp
@@ -25,82 +25,122 @@ size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
 
 int overload_counter = 0;
 
-//Only Free-User support
 plugin_status plugin_exec(plugin_input &inp, plugin_output &outp) {
 	ddcurl* handle = get_handle();
 	std::string resultstr;
 	string url = get_url();
 	resultstr.clear();
+	if(!inp.premium_user.empty() && !inp.premium_password.empty()) {
+		handle->setopt(CURLOPT_SSL_VERIFYPEER, 0);
+		std::string post_data = "user=" + inp.premium_user + "&pass=" + inp.premium_password;
+		handle->setopt(CURLOPT_URL, "https://www.share-online.biz/user/login");
+		handle->setopt(CURLOPT_REFERER, "http://www.share-online.biz/");
+		handle->setopt(CURLOPT_POST, 1);
+		handle->setopt(CURLOPT_COPYPOSTFIELDS, post_data.c_str());
+		handle->setopt(CURLOPT_FOLLOWLOCATION, 1);
+		handle->setopt(CURLOPT_COOKIEFILE, "");
+		handle->setopt(CURLOPT_COOKIE, "http://www.share-online.biz; page_language=english");
+		handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+		handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+		handle->perform();
 
-	handle->setopt(CURLOPT_COOKIE, "http://www.share-online.biz; page_language=english");
-	handle->setopt(CURLOPT_URL, url.c_str());
-	handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
-	handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+		if(resultstr.find("login_error") != string::npos)
+			return PLUGIN_AUTH_FAIL;
+		handle->setopt(CURLOPT_POST, 0);
+		handle->setopt(CURLOPT_HEADER, 0);
+		handle->setopt(CURLOPT_URL, url);
+		handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+		handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+		int success = handle->perform();
 
-	int success = handle->perform();
+		if(success != 0) {
+			return PLUGIN_CONNECTION_ERROR;
+		}
 
-	// if IP block from Server wait 10min
-	if (resultstr.find("Share-Online - Access to content denied") != std::string::npos){
-		overload_counter = 0;
-		set_wait_time(600);
-		return PLUGIN_SERVER_OVERLOADED;
-	}
+		//is file deleted?
+		if (resultstr.find("dl_failure") != std::string::npos)
+			return PLUGIN_FILE_NOT_FOUND;
 
-	if(success != 0) {
-		return PLUGIN_CONNECTION_ERROR;
-	}
+		//get real link
+		string code = search_between(resultstr, "dl=\"","\"");
+		string data = base64_decode(code);
+		if (data.empty())
+			return PLUGIN_FILE_NOT_FOUND;
 
-	//is file deleted?
-	if (resultstr.find("The requested file is not available!") != std::string::npos)
-		return PLUGIN_FILE_NOT_FOUND;
-
-	//get free User Link
-	url = search_between(resultstr, "var url=\"","\"");
-	resultstr.clear();
-	handle->setopt(CURLOPT_POST,1);
-	handle->setopt(CURLOPT_COPYPOSTFIELDS, "dl_free=1&choice=free");
-	handle->setopt(CURLOPT_FOLLOWLOCATION, 1);
-	handle->setopt(CURLOPT_URL, url.c_str());
-	handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
-	handle->setopt(CURLOPT_WRITEDATA, &resultstr);
-	success = handle->perform();
-
-	if(success != 0) {
-		return PLUGIN_CONNECTION_ERROR;
-	}
-
-	//this can be true very often, somethimes you need to wait very long time to get a Downloadticket
-	//if downloadlink can't get at 30 retrys set waittime to 10 min to prevent IP block from server
-	if (resultstr.find("No free slots for free users!") != std::string::npos){
-		if (overload_counter < 30){
-			overload_counter++;
-			set_wait_time(3);
-			return PLUGIN_SERVER_OVERLOADED;
+		outp.download_url = data;
+		return PLUGIN_SUCCESS;
 		}
 		else{
+			handle->setopt(CURLOPT_COOKIE, "http://www.share-online.biz; page_language=english");
+			handle->setopt(CURLOPT_URL, url.c_str());
+			handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+			handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+
+			int success = handle->perform();
+
+			// if IP block from Server wait 10min
+			if (resultstr.find("Share-Online - Access to content denied") != std::string::npos){
+				overload_counter = 0;
+				set_wait_time(600);
+				return PLUGIN_SERVER_OVERLOADED;
+			}
+
+			if(success != 0) {
+				return PLUGIN_CONNECTION_ERROR;
+			}
+
+			//is file deleted?
+			if (resultstr.find("dl_failure") != std::string::npos)
+				return PLUGIN_FILE_NOT_FOUND;
+
+			//get free User Link
+			url = search_between(resultstr, "var url=\"","\"");
+			resultstr.clear();
+			handle->setopt(CURLOPT_POST,1);
+			handle->setopt(CURLOPT_COPYPOSTFIELDS, "dl_free=1&choice=free");
+			handle->setopt(CURLOPT_FOLLOWLOCATION, 1);
+			handle->setopt(CURLOPT_URL, url.c_str());
+			handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+			handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+			success = handle->perform();
+
+			if(success != 0) {
+				return PLUGIN_CONNECTION_ERROR;
+			}
+
+			//this can be true very often, somethimes you need to wait very long time to get a Downloadticket
+			//if downloadlink can't get at 30 retrys set waittime to 10 min to prevent IP block from server
+			if (resultstr.find("No free slots for free users!") != std::string::npos){
+				if (overload_counter < 30){
+					overload_counter++;
+					set_wait_time(3);
+					return PLUGIN_SERVER_OVERLOADED;
+				}
+				else{
+					overload_counter = 0;
+					set_wait_time(600);
+					return PLUGIN_SERVER_OVERLOADED;
+				}
+			}
+			//set back counter to 0
 			overload_counter = 0;
-			set_wait_time(600);
-			return PLUGIN_SERVER_OVERLOADED;
+
+			if (resultstr.find("This file is too big for you remaining download volume!") != std::string::npos){
+				set_wait_time(1800);
+				return PLUGIN_SERVER_OVERLOADED;
+			}
+			//wait-time until download can start
+			string wait = search_between(resultstr, "var wait=",";");
+			set_wait_time(atoi(wait.c_str()));
+
+			//get real link
+			string code = search_between(resultstr, "dl=\"","\"");
+			string data = base64_decode(code);
+
+			outp.download_url = data;
+			return PLUGIN_SUCCESS;
+
 		}
-	}
-	//set back counter to 0
-	overload_counter = 0;
-
-	if (resultstr.find("This file is too big for you remaining download volume!") != std::string::npos){
-		set_wait_time(1800);
-		return PLUGIN_SERVER_OVERLOADED;
-	}
-	//get real link
-	string code = search_between(resultstr, "dl=\"","\"");
-	string data = base64_decode(code);
-
-	//wait-time until download can start
-	string wait = search_between(resultstr, "var wait=",";");
-	set_wait_time(atoi(wait.c_str()));
-
-	outp.download_url = data;
-	return PLUGIN_SUCCESS;
-
 }
 bool get_file_status(plugin_input &inp, plugin_output &outp) {
 	string url = get_url();
@@ -157,5 +197,5 @@ extern "C" void plugin_getinfo(plugin_input &inp, plugin_output &outp) {
 		outp.allows_resumption = false;
 		outp.allows_multiple = false;
 	}
-	outp.offers_premium = false;
+	outp.offers_premium = true;
 }
