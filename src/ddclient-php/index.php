2c2468f0074a6097fe74358814ce8de1025cdd6f
b3nn0 <b3nn0@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2012-10-13 15:53:09 +0000

    fixed typos in ddclient-php. should work again now

--- src/ddclient-php/index.php
+++ src/ddclient-php/index.php
@@ -33,13 +33,13 @@ if(!isset($_SERVER['HTTPS']) || strtolower($_SERVER['HTTPS']) == "off") {
 }
 
 $err_message='';
-if(isset($_SERVER'HTTP_X_FORWARDED_SERVER'))
-	$host = $_SERVER'HTTP_X_FORWARDED_HOST';
+if(isset($_SERVER['HTTP_X_FORWARDED_SERVER']))
+	$host = $_SERVER['HTTP_X_FORWARDED_HOST'];
 else
-	$host = $_SERVER'HTTP_HOST';
+	$host = $_SERVER['HTTP_HOST'];
 
 $tpl_vars = array(
-	'T_SITE_URL' => $site_url . $host . substr($_SERVER'SCRIPT_NAME', 0, strrpos($_SERVER'SCRIPT_NAME', "/")+1),
+	'T_SITE_URL' => $site_url . $host . substr($_SERVER['SCRIPT_NAME'], 0, strrpos($_SERVER['SCRIPT_NAME'], "/")+1),
 	'T_DEFAULT_LANG' => LANG,
 	'L_DD' => $LANG['DD'],
 	'L_Manager' => $LANG['Manager'],
