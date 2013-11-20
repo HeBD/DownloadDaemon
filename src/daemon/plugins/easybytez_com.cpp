1b04e1794f2642b8d04ca041ea66223c6148031b
boy1979 <boy1979@01d1c00c-5677-49a0-a2e7-2dcf4399de71>
2013-11-20 11:29:07 +0000

    add support for reconnect and folders (easybytez.com)

--- src/daemon/plugins/easybytez_com.cpp
+++ src/daemon/plugins/easybytez_com.cpp
@@ -26,64 +26,105 @@ plugin_status plugin_exec(plugin_input &inp, plugin_output &outp) {
 	ddcurl* handle = get_handle();
 	std::string resultstr;
 	string url = get_url();
+    vector<string> splitted_url = split_string(url,"/");
 
-    handle->setopt(CURLOPT_POST,0);
-	handle->setopt(CURLOPT_URL, url.c_str());
-    handle->setopt(CURLOPT_COOKIEFILE, "");
-    handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
-	handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+    if (url.find("users") == string::npos) {
+        handle->setopt(CURLOPT_POST,0);
+        handle->setopt(CURLOPT_URL, url.c_str());
+        handle->setopt(CURLOPT_COOKIEFILE, "");
+        handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+        handle->setopt(CURLOPT_WRITEDATA, &resultstr);
 
-    int success = handle->perform();
+        int success = handle->perform();
 
-	if(success != 0) {
-		return PLUGIN_CONNECTION_ERROR;
-	}
+        if(success != 0) {
+            return PLUGIN_CONNECTION_ERROR;
+        }
+        vector<string> post_strings1 = search_all_between(resultstr, "input type=\"hidden\" name=", ">");
+        string post_string;
+        post_string.clear();
+        for(size_t i = 0; i < post_strings1.size(); ++i) {
+            vector<string> name_value = search_all_between(post_strings1[i], "\"", "\"");
+            post_string += name_value[0] + "=" + name_value[1] +"&";
+        }
+        post_string += "method_free=Free+Download";
+        resultstr.clear();
+        handle->setopt(CURLOPT_URL, url.c_str());
+        handle->setopt(CURLOPT_REFERER, url.c_str());
+        handle->setopt(CURLOPT_POST, 1);
+        handle->setopt(CURLOPT_COPYPOSTFIELDS, post_string);
+        handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+        handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+        handle->perform();
+        if (resultstr.find("<div class=\"err\">You have to wait ") != string::npos){
+            string wait_time=search_between(resultstr,"<div class=\"err\">You have to wait "," seconds till next download");
+            int Minutes=atoi(wait_time.substr(0,wait_time.find(" ")).c_str())*60;
+            int Seconds=atoi(wait_time.substr(wait_time.find(", ")+2,wait_time.length()-wait_time.find(", ")+2).c_str())+2;
+            set_wait_time(Minutes+Seconds);
+            return PLUGIN_SERVER_OVERLOADED;
+        }
+        string link_wait_time = search_between(resultstr, "<span id=\"countdown_str\">Wait","</span> seconds</span>");
+        link_wait_time = search_between(link_wait_time,">","<");
+        set_wait_time(atoi(link_wait_time.c_str()));
+        sleep(atoi(link_wait_time.c_str()));
+        vector<string> post_strings2 = search_all_between(resultstr, "input type=\"hidden\" name=", ">");
+        post_string.clear();
+        for(size_t i = 0; i < post_strings2.size(); ++i) {
+            vector<string> name_value = search_all_between(post_strings2[i], "\"", "\"");
+            post_string += name_value[0] + "=" + name_value[1] +"&";
+        }
+        post_string += "btn_download=Download+File";
+        resultstr.clear();
 
-    vector<string> post_strings1 = search_all_between(resultstr, "input type=\"hidden\" name=", ">");
-    string post_string;
-    post_string.clear();
-    for(size_t i = 0; i < post_strings1.size(); ++i) {
-        vector<string> name_value = search_all_between(post_strings1[i], "\"", "\"");
-        post_string += name_value[0] + "=" + name_value[1] +"&";
+        handle->setopt(CURLOPT_URL, url.c_str());
+        handle->setopt(CURLOPT_REFERER, url.c_str());
+        handle->setopt(CURLOPT_FOLLOWLOCATION, 0);
+        handle->setopt(CURLOPT_POST, 1);
+        handle->setopt(CURLOPT_HEADER, 1);
+        handle->setopt(CURLOPT_COPYPOSTFIELDS, post_string);
+        handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+        handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+        handle->perform();
+
+        handle->setopt(CURLOPT_POST, 0);
+        handle->setopt(CURLOPT_HEADER, 0);
+
+        outp.download_url = search_between(resultstr, "Location: ", "\r\n");
+        return PLUGIN_SUCCESS;
     }
-    post_string += "method_free=Free+Download";
-    resultstr.clear();
-    handle->setopt(CURLOPT_URL, url.c_str());
-    handle->setopt(CURLOPT_REFERER, url.c_str());
-    handle->setopt(CURLOPT_POST, 1);
-    handle->setopt(CURLOPT_COPYPOSTFIELDS, post_string);
-    handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
-    handle->setopt(CURLOPT_WRITEDATA, &resultstr);
-    handle->perform();
+    else
+    {
+        if (splitted_url.size() > 6){
+            download_container urls;
+            log_string("easybytez: receive Folder Links", LOG_DEBUG);
+            vector<string> links;
+            string new_url=url.c_str();
+            do{
+                resultstr.clear();
+                handle->setopt(CURLOPT_POST,0);
+                handle->setopt(CURLOPT_URL, new_url);
+                handle->setopt(CURLOPT_COOKIEFILE, "");
+                handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
+                handle->setopt(CURLOPT_WRITEDATA, &resultstr);
+                handle->perform();
+                links = search_all_between(resultstr, "<div class=\"link\"><a href=\"","\" target=\"_blank\">");
+                for(size_t i = 0; i < links.size(); i++) {
+                    log_string("easybytez: Add Link:" + links[i], LOG_DEBUG);
+                    urls.add_download(links[i], "");
+                }
+                size_t Found_Next=resultstr.find("'>Next ");
+                size_t Found_Link=resultstr.rfind("<a href='", Found_Next);
+                string page=resultstr.substr(Found_Link+9, Found_Next-Found_Link-9);
+                new_url=url.c_str()+page;
 
-    set_wait_time(60);
-    sleep(60);
-    vector<string> post_strings2 = search_all_between(resultstr, "input type=\"hidden\" name=", ">");
-    post_string.clear();
-    for(size_t i = 0; i < post_strings2.size(); ++i) {
-        vector<string> name_value = search_all_between(post_strings2[i], "\"", "\"");
-        post_string += name_value[0] + "=" + name_value[1] +"&";
-    }
-    post_string += "btn_download=Download+File";
-    resultstr.clear();
-
-    handle->setopt(CURLOPT_URL, url.c_str());
-    handle->setopt(CURLOPT_REFERER, url.c_str());
-    handle->setopt(CURLOPT_FOLLOWLOCATION, 0);
-    handle->setopt(CURLOPT_POST, 1);
-    handle->setopt(CURLOPT_HEADER, 1);
-    handle->setopt(CURLOPT_COPYPOSTFIELDS, post_string);
-    handle->setopt(CURLOPT_WRITEFUNCTION, write_data);
-    handle->setopt(CURLOPT_WRITEDATA, &resultstr);
-    handle->perform();
-
-
-    handle->setopt(CURLOPT_POST, 0);
-    handle->setopt(CURLOPT_HEADER, 0);
-
-    outp.download_url = search_between(resultstr, "Location: ", "\r\n");
-	return PLUGIN_SUCCESS;
-
+            } while(resultstr.find("'>Next ") != string::npos);
+            replace_this_download(urls);
+            return PLUGIN_SUCCESS;
+        }
+        else
+            log_string("easybytez: ERROR Receiving folder-content" , LOG_DEBUG);
+            return PLUGIN_ERROR;
+   }
 }
 
 extern "C" void plugin_getinfo(plugin_input &inp, plugin_output &outp) {
